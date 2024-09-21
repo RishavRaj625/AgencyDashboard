83cef1733ca7a63e3d190d7d8755ad21c7d11b75
RishavRaj625 <crishav938@gmail.com>
2024-09-21 23:28:15 +0530

    Initial commit

---
+++ overview.js
@@ -0,0 +1,5 @@
+// You can extend this script to handle user interactions, sorting, filtering, or anything else
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Dashboard loaded');
+    // Add any interactivity here
+});
